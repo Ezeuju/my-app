604fda7e049a25d082ac2a7c87e520f8c34e456b
Ezeuju <ezeuju1919@gmail.com>
2023-04-18 21:50:48 -0700

    completed todolist app

---
+++ src/components/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className='Todo'>
+      <div>
+      <p onClick={() => toggleComplete(task.id)}
+      className={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+      </div>
+      <div className='font'>
+      <FontAwesomeIcon className='edit' icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+      <FontAwesomeIcon className='trash' icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+      </div>
+    </div>
+  )
+}
+
+export default Todo
+
+    
